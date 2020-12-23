edaf0a02b2ef0c7ac038761fa9d10fd792e66c00
yuanjun <861022418@qq.com>
2020-12-23 13:14:38 +0800

    修改文件名

--- 2.Python运维/2.业务服务监控/1.3 对比Nginx配置文件差异.py
+++ 2.Python运维/2.业务服务监控/1.3 对比Nginx配置文件差异.py
@@ -34,4 +34,4 @@ if __name__ == "__main__":
     d = difflib.HtmlDiff()
     print(d.make_file(text1_lines, text2_lines))
     # 终端执行命令，将结果输出到 diffFile.html文件
-    # python 1.例-对比Nginx配置文件差异.py 1._Differ-比较2个字符串差异.py 1._HtmlDiff-生成对比html文档.py > diffFile.html
+    # python 1.3 对比Nginx配置文件差异.py 1.1 Differ-比较2个字符串差异.py 1.2 HtmlDiff-生成对比html文档.py > diffFile.html
--- 2.Python运维/2.业务服务监控/2.1 filecmp.cmp单文件差异.py
+++ 2.Python运维/2.业务服务监控/2.1 filecmp.cmp单文件差异.py
@@ -2,10 +2,10 @@ import filecmp
 
 if __name__ == "__main__":
     print(filecmp.cmp(
-        "/Users/yuanjun/Documents/priv_repo/python-notebook/2.Python运维/2.业务服务监控/1.例-对比Nginx配置文件差异.py",
+        "/2.Python运维/2.业务服务监控/1.3 对比Nginx配置文件差异.py",
         "/Users/yuanjun/Documents/priv_repo/python-notebook/2.Python运维/2.业务服务监控/1.difflib-文件内容差异对比.md"))
 
     print(filecmp.cmp(
-        "/Users/yuanjun/Documents/priv_repo/python-notebook/2.Python运维/2.业务服务监控/1.例-对比Nginx配置文件差异.py",
+        "/2.Python运维/2.业务服务监控/1.3 对比Nginx配置文件差异.py",
         "/Users/yuanjun/Documents/priv_repo/python-notebook/2.Python运维/2.业务服务监控/1.difflib-文件内容差异对比.md",
         shallow=False))
